a6c50cd3e668da832f5f90ebeff18ed0f595f626
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-07-16 23:20:04 +0200

    refactor: move peval_file utils to duk_util

--- src/duk_util.c
+++ src/duk_util.c
@@ -1,5 +1,55 @@
 
 #include "duk_util.h"
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int duk_util_peval_commonjs_file(duk_context *ctx, const char *filepath) {
+  int result;
+
+  duk_push_object(ctx);
+  duk_put_global_string(ctx, "module");
+
+  result = duk_util_peval_file(ctx, filepath);
+  if (result != 0) {
+    return -1;
+  }
+
+  duk_pop(ctx);
+  duk_get_global_string(ctx, "module");
+  duk_get_prop_string(ctx, -1, "exports");
+  duk_insert(ctx, -2);
+  duk_pop(ctx);
+  return 0;
+}
+
+int duk_util_peval_file(duk_context *ctx, const char *filepath) {
+  int fid, file_size, result;
+  const char *file_contents;
+
+  fid = open(filepath, O_RDONLY);
+  if (fid < 0) {
+    return -1;
+  }
+
+  file_size = lseek(fid, 0, SEEK_END);
+  if (file_size < 0) {
+    close(fid);
+    return -1;
+  }
+
+  file_contents = mmap(0, file_size, PROT_READ, MAP_PRIVATE, fid, 0);
+  if (file_contents == MAP_FAILED) {
+    close(fid);
+    return -1;
+  }
+
+  result = duk_peval_string(ctx, file_contents);
+
+  munmap((void *)file_contents, file_size);
+  close(fid);
+  return result;
+}
 
 int duk_util_unroll_array(duk_context *ctx) {
   int enum_index, count = 0;
--- src/duk_util.h
+++ src/duk_util.h
@@ -2,5 +2,7 @@
 
 #include "duktape.h"
 
+int duk_util_peval_commonjs_file(duk_context *ctx, const char *filepath);
+int duk_util_peval_file(duk_context *ctx, const char *filepath);
 int duk_util_unroll_array(duk_context *ctx);
 void duk_util_put_first_prop_name(duk_context *ctx);
